dfd19df5e334e85f0ec6ba211b12158ab83890f8
Tyler Popson <nbcomplete@pm.me>
2023-11-04 00:42:12 -0400

    got a very bare bones layout working, needs refinement.

--- src/networking-project/src/UserInterface.java
+++ src/networking-project/src/UserInterface.java
@@ -2,17 +2,47 @@ import javax.swing.*;
 import java.awt.*;
 
 public class UserInterface extends JFrame {
-    private Canvas drawArea;
+    private  JPanel drawArea;
+    private Canvas canvas;
     private JPanel controls;
+    private JButton connect;
+    private JLabel connuctionStatus;
+    private JLabel timeLimit;
+    private JLabel prompt;
+    private JButton submit;
 
     UserInterface(){
-        drawArea = new Canvas();
+        drawArea = new JPanel();
+        canvas = new Canvas();
         controls = new JPanel();
+        connect = new JButton("connect");
+        connuctionStatus = new JLabel("disconnected");
+        timeLimit = new JLabel("0:00");
+        prompt = new JLabel("Prompt");
+        submit = new JButton("submit");
 
+        canvas.setBackground(new Color(0.6f,0.6f,0.6f));
+        canvas.setSize(new Dimension(500, 500));
 
+        drawArea.add(canvas);
 
-        add(drawArea);
-        setSize(800, 500);
+//      Configure the controls and info pannel
+        controls.setLayout(new BoxLayout(controls, BoxLayout.PAGE_AXIS));
+        controls.setSize(300, 500);
+
+        controls.add(connect);
+        controls.add(connuctionStatus);
+        controls.add(timeLimit);
+        controls.add(prompt);
+        controls.add(submit);
+
+//      configure the main window
+        setLayout(new BorderLayout(10, 5));
+
+        add(drawArea, BorderLayout.CENTER);
+        add(controls, BorderLayout.EAST);
+
+        setSize(new Dimension(800, 500));
         setVisible(true);
 
     }
