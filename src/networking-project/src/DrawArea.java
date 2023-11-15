7b2bef3d1676de7761db776461b675bd3e2f9504
Tyler Popson <tylerpopson@gmail.com>
2023-11-14 21:17:07 -0500

    got painting on panel to work

--- src/networking-project/src/DrawArea.java
+++ src/networking-project/src/DrawArea.java
@@ -4,17 +4,25 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
 public class DrawArea extends JPanel implements MouseMotionListener {
+    int x = -1;
+    int y = -1;
     DrawArea(){
         addMouseMotionListener(this);
     }
 
     @Override
     public void mouseDragged(MouseEvent mouseEvent) {
-        System.out.println(mouseEvent.getX() + ", " + mouseEvent.getY());
+        x = mouseEvent.getX();
+        y = mouseEvent.getY();
+        repaint();
     }
 
     @Override
     public void mouseMoved(MouseEvent mouseEvent) {
 
+    }
+
+    public void paintComponent(Graphics g){
+        g.fillOval(x,y,5,5);
     }
 }
