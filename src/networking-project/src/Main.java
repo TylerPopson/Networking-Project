74af1bfdc513285f5f120dc694e0ba8a2efc96d9
zacharywray <zacharywray@gmail.com>
2023-11-30 17:52:43 -0500

    Created peer and host files. THe peer must be started first, then that host should be able to initiate a session until termination.

--- src/networking-project/src/Main.java
+++ src/networking-project/src/Main.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        //UserInterface ui = new UserInterface();
+        public static void main(String[] args) throws IOException {
+            UserInterface ui = new UserInterface();
+            // ui.CreateGuessMenu();
     }
 }
