24da8cb7cfda532dc49640c81aff758ca7e0d25f
Tyler Popson <tylerpopson@gmail.com>
2023-12-02 19:20:13 -0500

    finished form pending connection methods and main ui refactor

--- src/networking-project/src/ConnectUI.java
+++ src/networking-project/src/ConnectUI.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ConnectUI {
     private JTextField ipInput;
@@ -7,12 +10,38 @@ public class ConnectUI {
     private JButton cancelButton;
     private JLabel ipLabel;
     private JLabel error;
+    private JPanel ConnectUI;
+
+    public ConnectUI() {
+        connectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                boolean connectionError = false;
+
+                //attempt connect
+
+                if (connectionError){
+                    error.setVisible(true);
+                }else {
+                    // start game and close connection window
+                    
+                }
+            }
+        });
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //close program
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("ConnectUI");
-        frame.setContentPane(new ConnectUI().contentPane);
+        frame.setContentPane(new ConnectUI().ConnectUI);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
+
 }
--- src/networking-project/src/Main.java
+++ src/networking-project/src/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        ConnectUI.main(args);
         //UserInterface ui = new UserInterface();
         // ui.CreateGuessMenu()
     }
