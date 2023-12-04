b7fb36737793d8cfae11c50353c9fe090f92a18d
Zachary Nelson <zacharywray@gmail.com>
2023-12-04 17:07:09 -0500

    12/4/23

--- src/networking-project/src/Host.java
+++ src/networking-project/src/Host.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -13,16 +10,41 @@ public class Host {
     private Socket hostSocket; //Peer to reply to.
     private PrintWriter out;
     private BufferedReader in;
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
 
     public String sendMessage(String prompt) throws IOException {
         out.println(prompt);
         return in.readLine();
     }
+    void sendFile(String path) throws FileNotFoundException, IOException {
+    int bytes = 0;
+    //open the file with designated path.
+        File file = new File(path);
+        FileInputStream fileIStream = new FileInputStream(file);
+
+        //send file to server
+        dataOutputStream.writeLong(file.length());
+        //Buffer file
+        byte[] buffer = new byte [4 * 1024];
+        while ((bytes = fileIStream.read(buffer))
+                != -1){
+            //send file over socket.
+            dataOutputStream.write(buffer, 0, bytes);
+            dataOutputStream.flush();
+        }
+        // close the file here
+        fileIStream.close();
+    }
+
 
     public void initConnection(String ip, int port) throws IOException {
         hostSocket = new Socket(ip, port); //treat this host as a client for now.
         out = new PrintWriter(hostSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(hostSocket.getInputStream()));
+        //Get stream for file transfer.
+        dataInputStream = new DataInputStream (hostSocket.getInputStream());
+        dataOutputStream = new DataOutputStream(hostSocket.getOutputStream());
     }
         public void cutConnection() throws IOException {
             hostSocket.close();
@@ -30,13 +52,15 @@ public class Host {
 
     public static void main(String[] args) throws IOException {
         Host host = new Host();
-        host.initConnection("149.159.255.89", 6666);
+        host.initConnection("127.0.0.1", 7777);
         //Three-way handshake.
         String response = host.sendMessage("hello peer");
         System.out.println(response);
         //send the terminating char.
+        host.sendFile("C:/Users/Zach/IdeaProjects/Networking-Project/src/networking-project/test.png");
         response = host.sendMessage(".");
         System.out.println(response);
+
         //Tear down the connection.
         host.cutConnection();
 
--- src/networking-project/src/Peer.java
+++ src/networking-project/src/Peer.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,8 +18,10 @@ public class Peer {
     private Socket hostSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
 
-    public void initConnection(int port) throws IOException {
+    public void initConnection(int port) throws Exception {
         peerSocket = new ServerSocket(port);
         hostSocket = peerSocket.accept();
         out = new PrintWriter(hostSocket.getOutputStream(), true); //Get the outputted stream from the connected host.
@@ -28,8 +32,13 @@ public class Peer {
         } else {
             out.println("Unrecognized greeting");
         }
+        dataInputStream = new DataInputStream(
+                hostSocket.getInputStream());
+        dataOutputStream = new DataOutputStream(
+                hostSocket.getOutputStream());
         //Start the session.
         String inputLine;
+        receiveFile("test.png");
         while ((inputLine = in.readLine()) != null) {
             if (".".equals(inputLine)) { //termination char.
                 out.println("good bye");
@@ -39,8 +48,25 @@ public class Peer {
         }
     }
 
-
-
+    private void receiveFile(String fileName)throws Exception  {
+        int bytes = 0;
+        FileOutputStream  fileOStream = new FileOutputStream (fileName);
+        long size
+                = dataInputStream.readLong(); // read file size
+        byte[] buffer = new byte[4 * 1024];
+        while (size > 0
+                && (bytes = dataInputStream.read(
+                buffer, 0,
+                (int)Math.min(buffer.length, size)))
+                != -1) {
+            // Here we write the file using write method
+            fileOStream.write(buffer, 0, bytes);
+            size -= bytes; // read upto file size
+        }
+        // Here we received file
+        System.out.println("File is Received");
+        fileOStream.close();
+    }
 
 
     /**
@@ -62,8 +88,10 @@ public class Peer {
         peerSocket.close();
 
     }
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         Peer helloHost=new Peer();
-        helloHost.initConnection(6666);
+        helloHost.initConnection(7777);
+        helloHost.receiveFile("test.png");
+
     }
 }
