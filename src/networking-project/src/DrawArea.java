ec221de0fab08a9f5be13dbcb2b3d264db98396b
Tyler Popson <nbcomplete@pm.me>
2023-12-04 14:26:39 -0500

    rigged up existing functionality

--- src/networking-project/src/DrawArea.java
+++ src/networking-project/src/DrawArea.java
@@ -1,7 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class DrawArea extends JPanel implements MouseMotionListener {
     int x = -1;
@@ -9,7 +13,16 @@ public class DrawArea extends JPanel implements MouseMotionListener {
     DrawArea(){
         addMouseMotionListener(this);
     }
-
+    public void saveImage(String name,String type) {
+        BufferedImage image = new BufferedImage(getWidth(),getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = image.createGraphics();
+        paint(g2);
+        try{
+            ImageIO.write(image, type, new File(name+"."+type));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void mouseDragged(MouseEvent mouseEvent) {
         x = mouseEvent.getX();
@@ -22,7 +35,9 @@ public class DrawArea extends JPanel implements MouseMotionListener {
 
     }
 
-    public void paintComponent(Graphics g){
-        g.fillOval(x,y,5,5);
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D) g;
+        Shape circleShape = new Ellipse2D.Double(x, y, 3, 3);
+        g2d.draw(circleShape);
     }
 }
--- src/networking-project/src/Main.java
+++ src/networking-project/src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ConnectUI ui = new ConnectUI();
+        UI ui = new UI();
         ui.init();
         // UserInterface ui = new UserInterface();
         // ui.CreateGuessMenu()
