eef37ebefb5adc9414cf40e41155f59d5013dd72
Zachary Nelson <zacharywray@gmail.com>
2023-12-05 19:47:17 -0500

    12/5/23

--- src/networking-project/src/Host.java
+++ src/networking-project/src/Host.java
@@ -1,8 +1,13 @@
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.net.*;
+import java.awt.image.*;
+import javax.imageio.*;
 import java.io.*;
-import java.net.Socket;
-
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 /**
  * Representation of a Host.
  * Connects to the statically defined peer in the network.
@@ -17,44 +22,80 @@ public class Host {
     private static DataInputStream dis = null;
 
     public String sendMessage(String prompt) throws IOException {
+        out = new PrintWriter(hostSocket.getOutputStream(), true);
         out.println(prompt);
         String a = in.readLine();
         return a;
     }
-    void sendFile(String path) throws FileNotFoundException, IOException {
-    int bytes = 0;
-    //open the file with designated path.
-        File file = new File(path);
-        FileInputStream fileIStream = new FileInputStream(file);
-
-        //send file to server
-        dos.writeLong(file.length());
-        //Buffer file
-        byte[] buffer = new byte [4 * 1024];
-        while ((bytes = fileIStream.read(buffer))
-                != -1){
-            //send file over socket.
-            dos.write(buffer, 0, bytes);
-            dos.flush();
-        }
-        // close the file here
-        fileIStream.close();
-    }
-    void sendImage (String path) throws FileNotFoundException, IOException {
-        System.out.println("Reading image from disk. ");
-        img = ImageIO.read(new File(path));
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        ImageIO.write(img, "png", baos);
-        baos.flush();
-
-        byte[] bytes = baos.toByteArray();
-        baos.close();
-        //Send the image.
-        dos.writeInt(bytes.length);
-        dos.write(bytes, 0, bytes.length);
-        //close the output.
-        System.out.println("Image sent");
+//    void sendFile(String path) throws FileNotFoundException, IOException {
+//    int bytes = 0;
+//    //open the file with designated path.
+//        File file = new File(path);
+//        FileInputStream fileIStream = new FileInputStream(file);
+//
+//        //send file to server
+//        dos.writeLong(file.length());
+//        //Buffer file
+//        byte[] buffer = new byte [4 * 1024];
+//        while ((bytes = fileIStream.read(buffer))
+//                != -1){
+//            //send file over socket.
+//            dos.write(buffer, 0, bytes);
+//            dos.flush();
+//        }
+//        // close the file here
+//        fileIStream.close();
+//    }
+//    void sendImage (String path) throws FileNotFoundException, IOException {
+//        System.out.println("Reading image from disk. ");
+//        img = ImageIO.read(new File(path));
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//
+//        ImageIO.write(img, "png", baos);
+//        baos.flush();
+//
+//        byte[] bytes = baos.toByteArray();
+//        baos.close();
+//        //Send the image.
+//        dos.writeInt(bytes.length);
+//        dos.write(bytes, 0, bytes.length);
+//        //close the output.
+//        System.out.println("Image sent");
+//    }
+    //will need to take a buffered image as a paramater.
+//    public void sendImage2 (String path) throws Exception{
+//        try {
+//            System.out.println("Reading image from disk.");
+//            img = ImageIO.read(new File(path));
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//
+//            ImageIO.write(img, "jpg", baos);
+//            baos.flush();
+//
+//            byte[] bytes = baos.toByteArray();
+//            baos.close();
+//
+//            System.out.println("Sending image to server. ");
+//
+//            OutputStream out = hostSocket.getOutputStream();
+//            DataOutputStream dos = new DataOutputStream(out);
+//
+//            dos.writeInt(bytes.length);
+//            dos.write(bytes, 0, bytes.length);
+//
+//            System.out.println("Image sent to server. ");
+//
+////            dos.close();
+////            out.close();
+//
+//        } catch (Exception e) {
+//            System.out.println("Exception: " + e.getMessage());
+//            hostSocket.close();
+//        }
+//    }
+    public void sendImage3 (String path) throws Exception{
+        TestHost hostS = new TestHost();
+        hostS.init();
     }
 
 
@@ -70,13 +111,13 @@ public class Host {
             hostSocket.close();
         }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         Host host = new Host();
         host.initConnection("127.0.0.1", 7777);
         //Three-way handshake.
         String response = host.sendMessage("hello peer");
         System.out.println(response);
-        host.sendImage("C:/Users/Zach/IdeaProjects/Networking-Project/src/networking-project/test.png");
+        host.sendImage3("C:/Users/Zach/IdeaProjects/Networking-Project/src/networking-project/test.png");
         //send the terminating char.
         response = host.sendMessage(".");
         System.out.println(response);
